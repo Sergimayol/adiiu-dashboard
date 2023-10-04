4b0e84ce9e9ef52e462e6bfccdb8c73a1be6c8ad
Sergi Mayol <sergi2808@gmail.com>
2023-10-04 19:15:09 +0200

    fix algunas cosas

--- server/core/db.py
+++ server/core/db.py
@@ -1,19 +1,11 @@
 import sqlite3
-from .config import DB_PATH, DB_SCHEMA_PATH
+from .config import DB_PATH
 
 
 def get_db():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
-
-
-def init_db():
-    conn = get_db()
-    with open(DB_SCHEMA_PATH) as f:
-        conn.executescript(f.read())
-    conn.commit()
-    conn.close()
 
 
 def execute_sql(sql, params=None, commit=True, fetchone=False, fetchall=False):
