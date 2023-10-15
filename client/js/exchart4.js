9d0412fd12ca22d9f1368783e46c82d8e32e95b5
AinaT9 <aturserrano@gmail.com>
2023-10-15 14:12:59 +0200

    added two charts

---
+++ client/js/exchart4.js
@@ -0,0 +1,29 @@
+function exchart4(data) {
+    Highcharts.chart("chart4", {
+      chart: {
+        type: 'bar',
+      },
+      title: {
+        text: "Artist with most streams song",
+      },
+      xAxis: {
+        categories: data.map(item => item[0]),
+        title: {
+          text: "Artist",
+        },
+      },
+      yAxis: {
+        title: {
+          text: "Number of streams",
+        },
+      },
+      legend: {
+        enabled: true,
+      },
+      series:[{
+        name: 'Streaming Numbers',
+        data: data.map(item => item[1]),
+      }]
+    });
+  }
+  
--- server/core/songs.py
+++ server/core/songs.py
@@ -18,7 +18,18 @@ def get_songs_releasedyear_vs_numsongs():
         "SELECT released_year, COUNT(*) AS numsongs FROM songs GROUP BY released_year HAVING released_year >= 2000 ORDER BY released_year ASC",
         fetchall=True,
     )
+def get_artists_with_most_streams():
+    return execute_sql(
+       "SELECT artist_name, SUM(streams) AS streams  FROM songs GROUP BY artist_name ORDER BY streams DESC LIMIT 0,10",
+        fetchall=True,
+    )
 
+def get_songs_with_most_streams():
+    return execute_sql(
+        #Problema con LOVE GROWS 
+       "SELECT track_name, SUM(streams) AS streams FROM songs GROUP BY track_name ORDER BY streams DESC  LIMIT 0,20",
+        fetchall=True,
+    )
 
 def get_artists_occurences():
     return execute_sql(
