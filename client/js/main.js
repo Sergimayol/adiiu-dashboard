8853fa10f84947e32cebbc85762247616a05adff
Sergimayol <sergi2808@gmail.com>
2023-10-15 16:04:28 +0200

    Habilitar cache desde cliente y formateo código

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,7 @@
 async function fetchAPI(url) {
   const response = await fetch(url, {
     method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    cache: "force-cache",
   });
   const data = await response.json();
   return data;
@@ -17,12 +15,11 @@ function loadChartsData() {
     "http://127.0.0.1:8000/songs/artist-moststreams",
     "http://127.0.0.1:8000/songs/songs-moststreams",
     "http://127.0.0.1:8000/songs/songs-weeknd",
-
   ];
 
   // Usar Promise.all() para manejar múltiples solicitudes en paralelo
   Promise.all(urls.map((url) => fetchAPI(url).then((data) => data.data)))
-    .then(([data1, data2, data3,data4,data5,data6]) => {
+    .then(([data1, data2, data3, data4, data5, data6]) => {
       exchart1(data1);
       exchart2(data2);
       exchart3(data3);
