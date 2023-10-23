82e4f63ef8b0a4a81e325191054bfb444237deae
Sergimayol <sergi2808@gmail.com>
2023-10-23 16:24:04 +0200

    format code + arreglo query con params

--- server/core/main.py
+++ server/core/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .songs import get_artists_occurences, get_songs, get_songs_mode_vs_streams, get_songs_releasedyear_vs_numsongs,get_artists_with_most_streams, get_songs_with_most_streams,get_songs_from_artist
+from .songs import (
+    get_artists_occurences,
+    get_songs,
+    get_songs_mode_vs_streams,
+    get_songs_releasedyear_vs_numsongs,
+    get_artists_with_most_streams,
+    get_songs_with_most_streams,
+    get_songs_from_artist,
+)
 
 app = FastAPI()
 origins = ["http://localhost:5000"]
@@ -55,12 +63,14 @@ async def artist_occurences():
 
     return {"data": data}
 
+
 @app.get("/songs/artist-moststreams")
 async def artist_moststreams():
     data = get_artists_with_most_streams()
     data = [[row[0], row[1]] for row in data]
 
     return {"data": data}
+
 
 @app.get("/songs/songs-moststreams")
 async def songs_moststreams():
@@ -69,8 +79,9 @@ async def songs_moststreams():
 
     return {"data": data}
 
+
 @app.get("/songs/{artist}")
-async def songs_artist(artist:str):
+async def songs_artist(artist: str):
     data = get_songs_from_artist(artist)
     data = [[row[0], row[1]] for row in data]
     return {"data": data}
--- server/core/songs.py
+++ server/core/songs.py
@@ -14,33 +14,35 @@ def get_songs_mode_vs_streams():
 
 def get_songs_releasedyear_vs_numsongs():
     return execute_sql(
-        # GET released year as unix timestamp
         "SELECT released_year, COUNT(*) AS numsongs FROM songs GROUP BY released_year HAVING released_year >= 2000 ORDER BY released_year ASC",
         fetchall=True,
     )
+
+
 def get_artists_with_most_streams():
     return execute_sql(
-       "SELECT artist_name, SUM(streams) AS streams  FROM songs GROUP BY artist_name ORDER BY streams DESC LIMIT 0,10",
+        "SELECT artist_name, SUM(streams) AS streams  FROM songs GROUP BY artist_name ORDER BY streams DESC LIMIT 0,10",
         fetchall=True,
     )
+
 
 def get_songs_with_most_streams():
     return execute_sql(
-        #Problema con LOVE GROWS 
-       "SELECT track_name, SUM(streams) AS streams FROM songs GROUP BY track_name ORDER BY streams DESC  LIMIT 0,20",
+        "SELECT track_name, SUM(streams) AS streams FROM songs GROUP BY track_name ORDER BY streams DESC  LIMIT 0,20",
         fetchall=True,
     )
 
+
 def get_songs_from_artist(artist):
-    artist_name = f"'%{artist}%'"
     return execute_sql(
-        " SELECT track_name, SUM(streams) FROM songs WHERE artist_name LIKE "+ artist_name+" GROUP BY track_name ORDER BY SUM(streams) DESC LIMIT 0,15",
-         fetchall=True,
+        "SELECT track_name, SUM(streams) FROM songs WHERE artist_name LIKE ? GROUP BY track_name ORDER BY SUM(streams) DESC LIMIT 0,15",
+        fetchall=True,
+        params=[f"%{artist}%"],
     )
+
 
 def get_artists_occurences():
     return execute_sql(
-        # The occurrences must be >=3
         "SELECT artist_name, COUNT(*) AS occurences FROM songs GROUP BY artist_name HAVING occurences >= 3 ORDER BY occurences DESC",
         fetchall=True,
     )
